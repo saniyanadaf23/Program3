3b7e3b31a40b28375f9d0c7a7acf44469be49d85
qanusha436 <amoghajoshi2005@gmail.com>
2025-05-06 14:23:49 +0530

    main branch

---
+++ Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+String name;
+int id;
+double salary;
+public Employee(String name,int id,double salary){
+    this.name = name;
+    this.id = id;
+    this.salary = salary;
+}
+public void displayInfo(){
+    System.out.println("Employee ID:"+id);
+    System.out.println("Employee Name:"+name);
+    System.out.println("Salary:$"+salary);
+}
+public static void main(String[] args) {
+    //creating an object of employee
+    Employee emp1 = new Employee("saniya",101,50000.0);
+    emp1.displayInfo();
+}
+}
